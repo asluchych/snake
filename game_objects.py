b2541b7971199a1f7f91bd8e1d692b75cb4e1af4
asluchych <anatolblr@gmail.com>
2022-12-31 15:19:32 +0100

    Create initial game structure

---
+++ game_objects.py
@@ -0,0 +1,19 @@
+import pygame as pg
+
+class Snake:
+    def __init__(self, game):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Food:
+    def __init__(self, game):
+        pass
+
+    def draw(self):
+        pass
