b6486681f689d4e482cc4d9025fcdb8a62df0cd3
asluchych <anatolblr@gmail.com>
2022-12-31 16:23:28 +0100

    Implement Food class

--- game_objects.py
+++ game_objects.py
@@ -34,11 +34,23 @@ class Snake:
     def get_random_position(self):
         return [randrange(self.size // 2, self.game.WINDOW_SIZE - self.size // 2, self.size)] * 2
 
+    def check_borders(self):
+        if self.rect.left < 0 or self.rect.right > self.game.WINDOW_SIZE:
+            self.game.new_game()
+        if self.rect.top < 0 or self.rect.bottom > self.game.WINDOW_SIZE:
+            self.game.new_game()
+
+    def check_food(self):
+        if self.rect.center == self.game.food.rect.center:
+            self.game.food.rect.center = self.get_random_position()
+
     def move(self):
         if self.delta_time():
             self.rect.move_ip(self.direction)
 
     def update(self):
+        self.check_borders()
+        self.check_food()
         self.move()
 
     def draw(self):
@@ -47,7 +59,10 @@ class Snake:
 
 class Food:
     def __init__(self, game):
-        pass
+        self.game = game
+        self.size = game.TILE_SIZE
+        self.rect = pg.rect.Rect([0, 0, game.TILE_SIZE - 2, game.TILE_SIZE - 2])
+        self.rect.center = self.game.snake.get_random_position()
 
     def draw(self):
-        pass
+        pg.draw.rect(self.game.screen, 'dimgray', self.rect)
